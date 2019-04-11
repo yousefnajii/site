df13017454d56283724a153170489957ece72c80
yousefnajii <49238962+yousefnajii@users.noreply.github.com>
2019-04-11 08:33:57 -0400

    changes v2

---
+++ JS/main.js
@@ -0,0 +1,20 @@
+function myFunction1(){
+	alert("Welcome");
+
+
+
+}
+
+function myFunction2(){
+	prompt("What's your name?");
+
+
+
+}
+
+function myFunction3(){
+
+	let userNumber = prompt("What's your number?");
+	alert("Hello " + userNumber + " is a good number");
+
+}
