d2aaa54dc0b22365019a5b2f53eeaba53a30c3eb
yousefnajii <49238962+yousefnajii@users.noreply.github.com>
2019-05-01 08:33:50 -0400

    js

--- JS/main.js
+++ JS/main.js
@@ -20,3 +20,23 @@ function myFunction3(){
 	alert("Hello " + userNumber + " is a good number");
 
 }
+
+
+function biggerNum(){
+
+var num1=parseInt(prompt("Enter first number"));
+var num2=parseInt(prompt("Enter second number"));
+
+	if (num1>num2){
+		console.log(num1+" is the bigger number");
+	}else if(num2>num1){
+		console.log(num2+" is the bigger number");
+	}else if(num1==num2){
+		console.log("The numbers are the same");
+	}else{
+		console.log("You didn't enter two numbers");
+	}
+}
+
+
+
